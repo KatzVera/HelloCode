5fedb4977c85e9b70bb2cc776f70c4c024c0b0d5
Katz Vera <Katz.VG@gmail.com>
2023-01-16 23:16:31 +0300

    Add comment

--- Exam009_IntroArray/Program.cs
+++ Exam009_IntroArray/Program.cs
@@ -7,8 +7,8 @@
 }
 //               0   1   2   3   4   5   6   7   8
 int[] array = { 11, 211, 31, 41, 15, 61, 17, 18, 19 };
-
-
+// array[0] = 12 - задать значение массива
+// Console.WriteLine(array[o]) - вывод в консоль значение массива по индексом "0"
 int result = Max(Max(array[0], array[1],array[2]), Max(array[3], array[4], array[5]), Max(array[6],array[7],array[8]));
 
 Console.WriteLine(result);
